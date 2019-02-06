efa3a348f018678c62adf4a8d50888bb6fff93fc
Luis Puhl <luispuhl@gmail.com>
2019-02-06 03:16:25 -0200

    Adding main form. [broken] on i18n for date picker.

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'package:tme_file_shr/models.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final title = 'Causando Impressão';
+
+  formNamePhone(context) {
+    return ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(20.0),
+      children: <Widget>[
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.person),
+            hintText: 'What do people call you?',
+            labelText: 'Nome *',
+          ),
+          onSaved: (String value) {
+            // This optional block of code can be used to run
+            // code when the user saves the form.
+          },
+          validator: (String value) {
+            return value.contains('@') ? 'Do not use the @ char.' : null;
+          },
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.phone),
+            hintText: 'What do people call you?',
+            labelText: 'Telefone *',
+          ),
+          onSaved: (String value) {
+            // This optional block of code can be used to run
+            // code when the user saves the form.
+          },
+          validator: (String value) {
+            return value.contains('@') ? 'Do not use the @ char.' : null;
+          },
+        ),
+        InputDecorator (
+          decoration: const InputDecoration(
+            icon: Icon(Icons.business),
+            labelText: 'Loja *',
+          ),
+          child: DropdownButton(
+            value: lojaStr[Loja.loja1],
+            onChanged: (l) => print(l),
+            items: lojaStr.values.map((str) => DropdownMenuItem<String>(
+              child: Text(str),
+              value: str,
+            )).toList(),
+          ),
+        ),
+        InputDecorator (
+          decoration: const InputDecoration(
+            icon: Icon(Icons.calendar_today),
+            labelText: 'Data Retirada',
+          ),
+          child: FlatButton(
+            child: Text(DateTime.now().toString()),
+            onPressed: () async {
+              final DateTime picked = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now().add(Duration(days: 3)),
+                firstDate: DateTime.now(),
+                lastDate: DateTime(2101)
+              );
+              print(picked);
+            },
+          ),
+        ),
+        Center(
+          child: Text('showDatePicker'),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      title: title,
+      // theme: ThemeData.dark(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('pt', ''),
+      ],
+      home: ScopedModel<Pedido>(
+        model: Pedido(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: ListView(
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(20.0),
+            children: <Widget>[
+              formNamePhone(context),
+              Text('Passo 2'),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
